a6b8bbf8d58e75c3c5b2f24b81ef7fe3a6fef6b3
Vincen <vincenhayate123@gmail.com>
2024-11-02 15:22:30 +0700

    Minor changes

--- resources/views/popular.blade.php
+++ resources/views/popular.blade.php
@@ -5,6 +5,8 @@
 <section class="m-4">
     <h2 class="fw-semibold pb-5">Popular</h2>
     @include('component.card', ['articles' => $articles])
-    {{ $articles->links() }}
+    <div class="d-flex justify-content-center">
+        {{ $articles->links() }}
+    </div>
 </section>
 @endsection
