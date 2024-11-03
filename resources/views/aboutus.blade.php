bf40b31c4d3af9f47a38f555f5e563ba9787ce61
Vincen <vincenhayate123@gmail.com>
2024-11-03 19:03:51 +0700

    Fix about us

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'About Us')
 
 @section('content')
-<section class="d-flex flex-column gap-4 m-4 text-center w-50 mx-auto" style="padding: 6rem 0 6rem 0;">
+<section class="d-flex flex-column gap-4 m-4 text-center w-50 mx-auto min-vh-100" style="padding: 6rem 0 6rem 0;">
     <h2 class="fw-semibold">About EduFun</h2>
     <p class="fs-5">EduFun adalah perusahaan pendidikan berbasis teknologi asal Indonesia. EduFun menyediakan layanan akses pendidikan dalam format tulisan berbahsa Indonesia yang disajikan secara online melalui website.</p>
     <p class="fs-5">Hingga Juni 2024, EduFun memiliki lebih dari 10 ribu pengguna. EduFun hadir sebagai bentuk revolusi dari pendidikan di Indonesia dengan mengedepankan cara berpikir kritis, logis, rasional, dan sumber pengetahuan sains yang terintegrasi terhadap semua mahasiswa IT di Indonesia. EduFun bercita-cita mencetak generasi Indonesia yang memahami ilmu pengetahuan dan cinta belajar.</p>
