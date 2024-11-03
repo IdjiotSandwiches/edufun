0cc906ee94692b265113159b85905d34324bff21
Vincen <vincenhayate123@gmail.com>
2024-11-03 19:20:18 +0700

    Add shadow to writer-card

--- resources/views/component/writer-card.blade.php
+++ resources/views/component/writer-card.blade.php
@@ -1,7 +1,7 @@
 @props(['writer'])
 
 <a href="{{ route('getWriterArticles', ['writer' => $writer->name]) }}" class="card bg-transparent border-0 text-center text-dark link-underline link-underline-opacity-0" style="width: 18rem;">
-    <img src="{{ asset($writer->avatar) }}" class="card-img-top img-fluid mx-auto d-block rounded-circle" alt="...">
+    <img src="{{ asset($writer->avatar) }}" class="card-img-top shadow img-fluid mx-auto d-block rounded-circle" alt="...">
     <div class="pt-4">
         <h5 class="card-title fw-semibold">{{ $writer->name }}</h5>
         <p class="card-text">{{ $writer->specialize }}</p>
